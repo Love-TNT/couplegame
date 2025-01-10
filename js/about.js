8093b4332e0a2ac464a8f76ef5e2846f77a58711
2,4,6三硝基甲苯 <88142364+Love-TNT@users.noreply.github.com>
2025-01-10 22:32:15 +0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,12 +1,11 @@
 window.unLockPositions = () => {
 
   let lockCode = $("#lockCode").val()
-
-  /*if (lockCode !='000000') {    
-    alert('识别码错误')
-    return
-  }*/
-
   localStorage.setItem('vip', true)
+  location.href = "./index.html"
+}
+window.rt = () => {
+
+  let lockCode = $("#lockCode").val()
   location.href = "./index.html"
 }
